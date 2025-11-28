abc47aedaeaba244f4c00a2001d3aa4669d8d9af
Matheus Ribeiro <matheus.jbr12345@gmail.com>
2025-11-28 00:39:01 -0300

    Create TelaEsqueceuSenha.java
    Tela de Esqueceu a Senha iniciada

---
+++ src/main/java/com/churrasqueiro/ui/TelaEsqueceuSenha.java
@@ -0,0 +1,43 @@
+package com.churrasqueiro.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class TelaEsqueceuSenha extends JFrame {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaEsqueceuSenha window = new TelaEsqueceuSenha();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TelaEsqueceuSenha() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1280, 720);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
