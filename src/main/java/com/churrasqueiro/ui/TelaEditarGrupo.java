cfb9f0771505c5585fb072bb88c0e8295ad5192c
VictorS <victor.user.scds@gmail.com>
2025-12-03 16:26:45 -0300

    Criação da tela editar grupo
    Feita a criação da tela editar grupo e toda seua forma no window builder

---
+++ src/main/java/com/churrasqueiro/ui/TelaEditarGrupo.java
@@ -0,0 +1,146 @@
+package com.churrasqueiro.ui;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.churrasqueiro.business.CategoriaController;
+import com.churrasqueiro.entities.Categoria;
+import com.churrasqueiro.entities.ItemCardapio;
+import com.churrasqueiro.exceptions.ControllerException;
+import com.churrasqueiro.exceptions.DatabaseException;
+
+public class TelaEditarGrupo extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private static final int LARGURA = 1280;
+    private static final int ALTURA = 720;
+    private EstilizacaoRedonda.CaixaTextoRedonda campoNomeGrupo;
+    private EstilizacaoRedonda.CaixaTextoRedonda campoDescricaoGrupo;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaEditarGrupo frame = new TelaEditarGrupo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+    Color corPaletaBege = new Color(227,202,187);
+    Color corPaletaVermelho = new Color(179,13,36);
+    Color corPaletaVermelhoInteracao = new Color(200,50,50);
+    Color corPaletaVermelhoPressionado = new Color(150,0,0);
+    Color corPaletaBranco = new Color(255,255,255);
+    Color corPaletaPreto = new Color(0,0,0);
+    Color corPaletaPretoInteração = new Color(35,35,35);
+    Color corPaletaCinza = new Color(140,127,127);
+
+	public TelaEditarGrupo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(LARGURA, ALTURA);
+		setTitle("Editar Grupo - Churrasqueiro");
+		setResizable(false);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setBackground(corPaletaVermelho);
+		
+		java.net.URL url = getClass().getResource("/assets/imagens/iconeJanela.png");
+        if (url != null) {
+            try {
+                java.awt.Image icon = javax.imageio.ImageIO.read(url);
+                setIconImage(icon);
+            } catch (java.io.IOException e) {
+                System.err.println("Falha de I/O ao ler a imagem: " + e.getMessage());
+            }
+        }
+        contentPane.setLayout(null);
+        
+        JLabel gruposLabel = new JLabel("Grupos");
+        gruposLabel.setBounds(539, 34, 208, 38);
+        gruposLabel.setFont(new Font("SansSerif", Font.BOLD, 36));
+        gruposLabel.setForeground(corPaletaBege);
+        contentPane.add(gruposLabel);
+        
+        JLabel logoLabel = new JLabel("");
+        logoLabel.setBounds(30, 10, 92, 82);
+        logoLabel.setIcon(new ImageIcon(TelaEditarGrupo.class.getResource("/assets/imagens/iconeJanelaPequena.png")));
+        contentPane.add(logoLabel);
+        
+        JPanel panel = new JPanel();
+        panel.setBounds(0, 102, 1266, 581);
+        panel.setBackground(corPaletaBege);
+        contentPane.add(panel);
+        panel.setLayout(null);
+        
+        JLabel editarGrupoLabel = new JLabel("Editar Grupo");
+        editarGrupoLabel.setBounds(504, 27, 353, 68);
+        panel.add(editarGrupoLabel);
+        editarGrupoLabel.setFont(new Font("SansSerif", Font.BOLD, 36));
+        editarGrupoLabel.setForeground(corPaletaVermelho);
+        
+        JLabel nomeLabel = new JLabel("Nome");
+        nomeLabel.setBounds(65, 145, 187, 32);
+        panel.add(nomeLabel);
+        nomeLabel.setFont(new Font("SansSerif", Font.BOLD, 20));
+        nomeLabel.setForeground(corPaletaPreto);
+        
+        this.campoDescricaoGrupo = new EstilizacaoRedonda.CaixaTextoRedonda("Digite o nome do Grupo",corPaletaVermelho,corPaletaBege,corPaletaCinza,2,35);
+        campoDescricaoGrupo.setFont(new Font("SansSerif", Font.BOLD, 14));
+        campoDescricaoGrupo.setBounds(65, 280, 1135, 38);
+		panel.add(campoDescricaoGrupo);
+		campoDescricaoGrupo.setColumns(10);
+		
+		this.campoNomeGrupo = new EstilizacaoRedonda.CaixaTextoRedonda("Digite a descrição do Grupo",corPaletaVermelho,corPaletaBege,corPaletaCinza,2,35);
+		campoNomeGrupo.setFont(new Font("SansSerif", Font.BOLD, 14));
+		campoNomeGrupo.setBounds(65, 179, 1135, 38);
+		panel.add(campoNomeGrupo);
+		campoNomeGrupo.setColumns(10);
+		
+		JLabel descricaoLabel = new JLabel("Descrição");
+		descricaoLabel.setForeground(Color.BLACK);
+		descricaoLabel.setFont(new Font("SansSerif", Font.BOLD, 20));
+		descricaoLabel.setBounds(65, 246, 187, 32);
+		panel.add(descricaoLabel);
+		
+		final EstilizacaoRedonda.BotaoRedondo botaoAtualizarGrupo = new EstilizacaoRedonda.BotaoRedondo("Atalizar Grupo",corPaletaPreto,corPaletaPretoInteração,corPaletaPreto,35);
+		botaoAtualizarGrupo.setText("Atualizar Grupo");
+		botaoAtualizarGrupo.setBounds(504, 445, 205, 38);
+		panel.add(botaoAtualizarGrupo);
+		botaoAtualizarGrupo.setFont(new Font("SansSerif", Font.BOLD, 18));
+		botaoAtualizarGrupo.setForeground(corPaletaBege);
+		botaoAtualizarGrupo.setBackground(new Color(0, 0, 0));
+		
+		
+		final EstilizacaoRedonda.BotaoRedondo botaoVoltar = new EstilizacaoRedonda.BotaoRedondo("Voltar",corPaletaPreto,corPaletaPretoInteração,corPaletaPreto,35);
+		botaoVoltar.setBounds(1128, 41, 104, 38);
+		contentPane.add(botaoVoltar);
+		botaoVoltar.setFont(new Font("SansSerif", Font.BOLD, 18));
+		botaoVoltar.setForeground(new Color(255, 255, 255));
+		botaoVoltar.setBackground(new Color(0, 0, 0));
+		botaoVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				TelaItens telaItens = new TelaItens();
+				telaItens.setVisible(true);
+			}
+		});
+	}
+}
