ae64df65a352a2728d76446ce1f6c42bf5bc3d53
Victor <victor.montes@montreal.com.br>
2025-12-13 11:08:47 -0300

    Separação H2 para chamar no Bat.

--- src/main/java/com/churrasqueiro/main/Main.java
+++ src/main/java/com/churrasqueiro/main/Main.java
@@ -5,6 +5,7 @@ import com.churrasqueiro.ui.TelaLogin;
 
 public class Main {
     public static void main(String[] args) {
+        H2Initializer.init();
 
         SwingUtilities.invokeLater(() -> {
             new TelaLogin().setVisible(true);
